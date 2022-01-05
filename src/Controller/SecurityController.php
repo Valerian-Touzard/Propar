4644ca07949ac9ae948e8928a6070d58694e3295
Preakaelis <vtouzard244@gmail.com>
2022-01-05 13:27:36 +0100

    Add security access
    Add security access

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -25,7 +25,7 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
+        //     return $this->redirectToRoute('operation_index');
         // }
 
         // get the login error if there is one
